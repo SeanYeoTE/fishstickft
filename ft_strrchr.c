3c377d37c36180fa4c2d1fa53cb3db8c55b2896e
Sean <tseensean@gmail.com>
2023-09-10 23:31:03 +0800

    edited ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -13,5 +13,14 @@
 
 char *strrchr(const char *s, int c)
 {
-	
+	int i;
+
+    i = ft_strlen(s);
+    while (i >= 0)
+    {
+        if (s[i] == c)
+            return ((char *)s + i);
+        i--;
+    }
+    return (NULL);
 }
--- libft.h
+++ libft.h
@@ -15,4 +15,8 @@ void 	*memset(void *s, int c, size_t n);
 void 	bzero(void *s, size_t n);
 void 	*memcpy(void *dest, const void *src, size_t n);
 
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	strlcat(char *dst, const char *src, size_t size);
+char    *strrchr(const char *s, int c);
+
 #endif
