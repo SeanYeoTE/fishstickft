8601aa2a70e0c520810c97bae5a52a53156be5fc
Sean Yeo <seayeo@c2r6s3.42singapore.sg>
2023-09-11 18:14:28 +0800

    added ft_substr

---
+++ ft_substr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seayeo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 17:31:12 by seayeo            #+#    #+#             */
+/*   Updated: 2023/09/11 17:31:13 by seayeo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start,size_t len)
+{
+	char *ans;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	ans =  (char *)malloc(sizeof(char *) * (len + 1));
+	if (ans == 0)
+		return (NULL);
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			str[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	str[j] = 0;
+	return (str);
+}
