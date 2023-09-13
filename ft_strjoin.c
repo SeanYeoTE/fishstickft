043c67c77ff75a5c7009b93db380d48cc2c52bf3
Sean <tseensean@gmail.com>
2023-09-13 12:32:10 +0800

    added ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+    int i;
+    int j;
+    char    *str;
+
+    i = 0;
+    j = 0;
+    str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(2) + 1));
+    if (str == NULL)
+        return (NULL);
+    while (s1[i] != '\0')
+    {
+        str[i] = s1[i];
+        i++;
+    }
+    while (s2[j] != '\0')
+    {
+        str[i] = s2[j];
+        i++;
+        j++;
+    }
+    str[i] = '\0';
+    return (str);
+}
