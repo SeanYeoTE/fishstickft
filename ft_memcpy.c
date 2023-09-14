06e7d8e565e4674bb257fa703a7c5bcffa403479
Sean <tseensean@gmail.com>
2023-09-14 12:31:36 +0800

    fixes

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+/*void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	char	*d;
 	char	*s;
@@ -26,4 +26,16 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 		i++;
 	}
 	return (dest);
+}*/
+void	*ft_memcpy(void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((char*)s1)[i] = ((char*)s2)[i];
+		i++;
+	}
+	return ((void*)s1);
 }
