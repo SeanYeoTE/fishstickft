7dc1e4c44daa75e577586a1494d2ceb1e2b99f48
Sean <tseensean@gmail.com>
2023-09-14 11:01:29 +0800

    norme fixes

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -11,10 +11,10 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-char    *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int	    i;
-	int	    j;
+	int		i;
+	int		j;
 	char	*str;
 
 	i = 0;
