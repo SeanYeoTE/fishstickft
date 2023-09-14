ed52d5c4cd5a8f079a9f61e776320fcba417a99c
Sean <tseensean@gmail.com>
2023-09-14 10:39:52 +0800

    norme fixes

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -13,26 +13,26 @@
 
 char *ft_strjoin(char const *s1, char const *s2)
 {
-    int i;
-    int j;
-    char    *str;
+	int	i;
+	int	j;
+	char	*str;
 
-    i = 0;
-    j = 0;
-    str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-    if (str == NULL)
-        return (NULL);
-    while (s1[i] != '\0')
-    {
-        str[i] = s1[i];
-        i++;
-    }
-    while (s2[j] != '\0')
-    {
-        str[i] = s2[j];
-        i++;
-        j++;
-    }
-    str[i] = '\0';
-    return (str);
+	i = 0;
+	j = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
 }
