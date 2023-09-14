3f5e132c88f6f3fec9e906810ae6ebb6dd80cada
Sean <tseensean@gmail.com>
2023-09-14 12:36:43 +0800

    null fix

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -11,12 +11,14 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-/*void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	char	*d;
 	char	*s;
 	size_t	i;
 
+	if (dest == NULL && src == NULL)
+		return (0);
 	d = (char *)dest;
 	s = (char *)src;
 	i = 0;
@@ -26,16 +28,4 @@
 		i++;
 	}
 	return (dest);
-}*/
-void	*ft_memcpy(void *s1, const void *s2, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < n)
-	{
-		((char *)s1)[i] = ((char *)s2)[i];
-		i++;
-	}
-	return ((void *)s1);
 }
