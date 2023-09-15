93b15cd0b52550a191871fd2d8c1dec2a915fa95
Sean <tseensean@gmail.com>
2023-09-16 00:55:14 +0800

    edited

--- libft.h
+++ libft.h
@@ -13,7 +13,7 @@ int		ft_strlen(const char *i);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t len);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
