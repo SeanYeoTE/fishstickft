8208f86edd4bc1f6c36ff2380f1e04df0c784ed6
Sean <tseensean@gmail.com>
2023-09-16 00:42:31 +0800

    edited

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,7 +12,7 @@
 #include "libft.h"
 
 int	ft_atoi(const char *str)
-{
+/*{
 	int		ans;
 	int		parity;
 	char	*tmp;
@@ -36,4 +36,27 @@ int	ft_atoi(const char *str)
 		tmp++;
 	}
 	return (ans * (parity * -1));
+}*/
+{
+	int	i;
+	int	neg;
+	int	res;
+
+	i = 0;
+	neg = 1;
+	res = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = (str[i] - '0') + (res * 10);
+		i++;
+	}
+	return (res * neg);
 }
