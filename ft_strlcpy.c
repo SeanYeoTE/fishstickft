e2a8f1ed8a33fef351f2d45b9caee7422edce119
Sean <tseensean@gmail.com>
2023-09-16 00:48:07 +0800

    test

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -12,7 +12,7 @@
 #include "libft.h"
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
-{
+/*{
 	size_t	i;
 	size_t	j;
 
@@ -28,5 +28,26 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 		i++;
 	}
 	dest[j] = '\0';
+	return (i);
+}*/
+{
+	size_t	i;
+
+	i = 0;
+	if (size == 0)
+	{
+		while (src[i])
+			i++;
+		return (i);
+	}
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < size)
+		dest[i] = '\0';
+	while (src[i] != '\0')
+		i++;
 	return (i);
 }
