e0078eb942e12d4a326b819e83b23e6cf55bc532
Sean Yeo <seayeo@c2r6s3.42singapore.sg>
2023-09-23 16:43:06 +0800

    norminette fix

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,29 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seayeo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/23 16:26:15 by seayeo            #+#    #+#             */
+/*   Updated: 2023/09/23 16:41:57 by seayeo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "libft.h"
 
-t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    t_list  *first;
-    t_list  *new;
+	t_list	*first;
+	t_list	*new;
 
-    if (!f || !del)
-        return (NULL);
-    first = NULL;
-    while (lst)
-    {
-        if (!(new = ft_lstnew((*f)(lst -> content))))
-        {
-            while (first)
-            {
-                new = first -> next;
-                (*del)(first -> content);
-                free(first);
-                first = new;
-            }
-            lst = NULL;
-            return (NULL);
-        }
-        ft_lstadd_back(&first, new);
-        lst = lst -> next;
-    }
-    return (first);
+	if (!f || !del)
+		return (NULL);
+	first = NULL;
+	while (lst)
+	{
+		new = ft_lstnew((*f)(lst -> content));
+		if (!new)
+		{
+			while (first)
+			{
+				new = first -> next;
+				(*del)(first -> content);
+				free(first);
+				first = new;
+			}
+			lst = NULL;
+			return (NULL);
+		}
+		ft_lstadd_back(&first, new);
+		lst = lst -> next;
+	}
+	return (first);
 }
