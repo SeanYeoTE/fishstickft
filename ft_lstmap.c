45e296c2062075ead7bfe89fef8fc362f74ef35b
Sean Yeo <seayeo@c2r6s3.42singapore.sg>
2023-09-25 11:16:26 +0800

    strict fixed

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -15,27 +15,24 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*first;
 	t_list	*new;
+	void	*ptr;
 
 	if (!f || !del)
 		return (NULL);
 	first = NULL;
 	while (lst)
 	{
-		new = ft_lstnew((*f)(lst -> content));
+		ptr = f(lst -> content);
+		new = ft_lstnew(ptr);
 		if (!new)
 		{
-			while (first)
-			{
-				new = first -> next;
-				(*del)(first -> content);
-				free(first);
-				first = new;
-			}
-			lst = NULL;
+			del(ptr);
+			ft_lstclear(&first, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&first, new);
 		lst = lst -> next;
 	}
+	lst = NULL;
 	return (first);
 }
