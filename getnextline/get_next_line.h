60f5e085c3adb61374506c0de68ed31a4c5568aa
Sean Yeo <seayeo@c2r4s4.42singapore.sg>
2023-10-09 16:29:55 +0800

    start gnl and printf eval

---
+++ getnextline/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seayeo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 16:31:03 by seayeo            #+#    #+#             */
+/*   Updated: 2023/10/05 16:31:03 by seayeo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <string.h>
+# include <limits.h>
+# include "./libft/libft.h"
