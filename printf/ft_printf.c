297ac669513d278f3fefe514b701483c2146e5d2
Sean Yeo <seayeo@c2r5s3.42singapore.sg>
2023-10-11 16:27:01 +0800

    should work now

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -11,48 +11,6 @@
 /* ************************************************************************** */
 #include "ft_printf.h"
 
-<<<<<<< HEAD
-=======
-int	print_char(int c)
-{
-	return (write(1, &c, 1));
-}
-
-int	print_str(char *str)
-{
-	int	count;
-
-	count = 0;
-	while (*str != '\0')
-	{
-		print_char((int)*str);
-		++count;
-		++str;
-	}
-	return (count);
-}
-
-int	print_digit(long n, int base)
-{
-	int		count;
-	char	*symbols;
-
-	symbols = "0123456789abcdef";
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		return print_digit(-n, base) + 1;
-	}
-	else if (n < base)
-		return (print_char(symbols[n]));
-	else
-	{
-		count = print_digit(n / base, base);
-		return count + print_digit(n % base, base);
-	}
-}
-
->>>>>>> aee60af7ba446c401d5fcfbd3fe7b5b7f220fe01
 int	print_format(char specifier, va_list ap)
 {
 	int	count;
