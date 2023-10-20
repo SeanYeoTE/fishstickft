3cafbecc71bfee5938d0d8e2f34c50326965d448
Sean <tseensean@gmail.com>
2023-10-20 16:01:28 +0800

    waloa

--- getnextline/get_next_line_utils.c
+++ getnextline/get_next_line_utils.c
@@ -105,6 +105,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
+	if (!s1)
+	{
+		s1 = malloc(1);
+		s1[0] = '\0';
+	}
 	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (str == NULL)
 		return (NULL);
