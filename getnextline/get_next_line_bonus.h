79d937595dcdb72fab86d75f6ad6d0ff7fbfae5f
Sean <tseensean@gmail.com>
2023-10-20 16:20:23 +0800

    test

---
+++ getnextline/get_next_line_bonus.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seayeo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 16:31:03 by seayeo            #+#    #+#             */
+/*   Updated: 2023/10/05 16:31:03 by seayeo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <string.h>
+# include <limits.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+
+# endif
+
+size_t	ft_strlen(const char *i);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+
+char	*readline(int fd, char *ret);
+char	*ft_replaceline(char *buf);
+char	*get_next_line(int fd);
+
+# endif
