4850e5b79f9d45b81c4213e0c5c6eb322f2babe7
Sean <tseensean@gmail.com>
2023-10-20 16:03:23 +0800

    test

--- getnextline/get_next_line.h
+++ getnextline/get_next_line.h
@@ -30,7 +30,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strcpy(char *dst, const char *src);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 
 char	*readline(int fd, char *ret);
