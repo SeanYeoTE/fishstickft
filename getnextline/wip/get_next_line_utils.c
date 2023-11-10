729f92fde745f8cdc10ec1f5fc70dd97c2e57729
Sean <tseensean@gmail.com>
2023-11-10 15:14:52 +0800

    idk

--- getnextline/wip/get_next_line_utils.c
+++ getnextline/wip/get_next_line_utils.c
@@ -41,33 +41,6 @@ int ft_strchr(const char *s, int c)
 	return (-1);
 }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*str;
-	size_t	s_len;
-	size_t	i;
-
-	s_len = ft_strlen(s);
-	i = 0;
-	if (!s)
-		return (NULL);
-	if (start >= s_len)
-	{
-		str = (char *)malloc(1);
-		if (str == NULL)
-			return (NULL);
-		str[0] = '\0';
-		return (str);
-	}
-	str = (char *)malloc(len + 1);
-	if (str == NULL)
-		return (NULL);
-	while (i++ < start)
-		s++;
-	ft_strlcpy(str, s, len + 1);
-	return (str);
-}
-
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
@@ -111,4 +84,27 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	while (src[i] != '\0')
 		i++;
 	return (i);
+}
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char	*ret;
+
+	if (!s)
+		return (0);
+	if (start > (size_t)ft_strlen(s))
+	{
+		ret = (char *)malloc(1);
+		if (!ret)
+			return (NULL);
+		ret[0] = '\0';
+		return (ret);
+	}
+	if (len > (size_t)(ft_strlen(s) - start))
+		len = ft_strlen(s) - start;
+	ret = malloc(sizeof(char) * (len + 1));
+	if (!ret)
+		return (0);
+	ft_strlcpy(ret, s + start, len + 1);
+	return (ret);
 }
