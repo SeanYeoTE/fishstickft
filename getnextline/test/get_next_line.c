6387e66eb6fadba7df7dc1fad397ba56748d3df3
Sean Yeo <seayeo@c2r5s1.42singapore.sg>
2023-11-14 16:21:08 +0800

    wow finally got smth

---
+++ getnextline/test/get_next_line.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seayeo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 16:30:28 by seayeo            #+#    #+#             */
+/*   Updated: 2023/11/06 13:49:39 by seayeo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+char	*joiner(char *store, char *buf)
+{
+	char	*ans;
+
+	ans = ft_strjoin(store, buf);
+	return (ans);
+}
+
+char	*read_to_storage(int fd, char *store)
+{
+	char	*buf;
+	int 	ans;
+
+	if (!store)
+		store = ft_calloc(1, 1);
+	buf = malloc(BUFFER_SIZE + 1);
+	if (!buf)
+		return (NULL);
+	ans = 1;
+	while (ans > 0)
+	{
+		ans = read(fd, buf, BUFFER_SIZE);
+		if (ans == -1)
+		{
+			free(buf);
+			return (NULL);
+		}
+		buf[ans] = '\0';
+		store = joiner(store, buf);
+		if (ft_strchr(buf, '\n') != -1)
+			break ;
+	}
+	free(buf);
+	return (store);
+}
+	
+char	*extract_line(char *buf)
+{
+	char	*line;
+	int		i;
+	int		ptr;
+
+	i = 0;
+	if (!buf[i])
+		return (NULL);
+	i = ft_strchr(buf, '\n');
+	ptr = -1;
+	if (i != -1)
+	{
+		line = ft_calloc(i + 2, sizeof(char));
+		while (++ptr < i)
+			line[ptr] = buf[ptr];
+		line[ptr++] = '\n';
+	}
+	else
+	{
+		line = ft_calloc(ft_strlen(buf) + 1, sizeof(char));
+		while(++ptr < (int)ft_strlen(buf))
+			line[ptr] = buf[ptr];
+	}
+	return (line);
+}
+
+
+char	*remains(char *buf)
+{
+	char	*leftover;
+	int		cutoff;
+	int 	newindexer;
+
+	cutoff = 0;
+	/*cutoff = ft_strchr(buf, '\n');*/
+	while (buf[cutoff] && buf[cutoff] != '\n')
+		cutoff++;
+	if (!buf[cutoff])
+		return (free(buf), NULL);
+	leftover = ft_calloc((ft_strlen(buf) - cutoff + 1), sizeof(char));
+	newindexer = 0;
+	cutoff++;
+	while (buf[cutoff])
+		leftover[newindexer++] = buf[cutoff++];
+	return (free(buf), leftover);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buf[4096];
+	char		*line;
+
+	if (fd < 0 || fd > 4095 || BUFFER_SIZE < 0)
+		return (NULL);
+	buf[fd] = read_to_storage(fd, buf[fd]);
+	if (!buf[fd])
+		return (NULL);
+	line = extract_line(buf[fd]);
+	buf[fd] = remains(buf[fd]);
+	return (line);
+}
---
+++ getnextline/test/get_next_line_utils.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seayeo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 15:32:13 by seayeo            #+#    #+#             */
+/*   Updated: 2023/11/14 15:32:21 by seayeo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *i)
+{
+	size_t	ct;
+
+	ct = 0;
+	while (i[ct])
+		ct++;
+	return (ct);
+}
+
+int ft_strchr(const char *s, int c)
+{
+	int		i;
+
+	i = 0;
+	if (!s)
+		return (-1);
+	while (s[i] != '\0')
+	{
+		if (s[i] == (unsigned char)c)
+			return (i);
+		i++;
+	}
+	if ((unsigned char)c == '\0')
+		return (i);
+	return (-1);
+}
+
+void	*ft_calloc(size_t nmemb, size_t	size)
+{
+	void	*ptr;
+
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, nmemb * size);
+	return (ptr);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		str[i] = '\0';
+		i++;
+	}
+}
+
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	if (!s1)
+	{
+		s1 = malloc(1);
+		s1[0] = '\0';
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	return (free(s1), str);
+}
