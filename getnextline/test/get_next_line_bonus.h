4d1d80a0f5fe4c3eae315e01c41134cfbc62c7c7
Sean Yeo <seayeo@c1r4s4.42singapore.sg>
2023-11-16 17:49:34 +0800

    finally i am finished

--- getnextline/test/get_next_line_bonus.h
+++ getnextline/test/get_next_line_bonus.h
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seayeo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:31:03 by seayeo            #+#    #+#             */
-/*   Updated: 2023/11/16 17:47:11 by seayeo           ###   ########.fr       */
+/*   Updated: 2023/11/16 17:48:09 by seayeo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <unistd.h>
 # include <stdlib.h>
