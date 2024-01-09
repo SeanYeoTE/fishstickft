104eccd8a3b619338b677ef58eaa48d14671841c
Sean Yeo <seayeo@c2r2s5.42singapore.sg>
2024-01-09 14:49:35 +0800

    fix for buffer size definition

--- getnextline/get_next_line_bonus.h
+++ getnextline/get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: seayeo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:31:03 by seayeo            #+#    #+#             */
-/*   Updated: 2023/11/16 17:48:09 by seayeo           ###   ########.fr       */
+/*   Updated: 2024/01/09 14:49:26 by seayeo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef GET_NEXT_LINE_BONUS_H
@@ -15,6 +15,10 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <limits.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1000
+#endif
 
 size_t	ft_strlen(const char *i);
 char	*ft_strchr(const char *s, int c);
