d513b0f060a1f828f56217bd23e142e654a80c32
Sean <tseensean@gmail.com>
2024-01-10 19:03:25 +0800

    learning still

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -14,6 +14,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 /*int	main(int argc, char *argv[])
 {
@@ -32,29 +33,64 @@
 
 	}
 }*/
+void	child(void)
+{
+	printf("\e[36mChild: Hi! I'm a child. I'm in an infinite loop.\e[0m\n");
+	while (1)
+		continue ;
+}
+
+void	kill_and_get_children(pid_t *pid)
+{
+	int	status;
+	int	i;
+
+	printf("Parent: I'm the murderous parent.\n");
+	i = 0;
+	while (i < 3)
+	{
+		kill(pid[i], SIGKILL);
+		i++;
+	}
+	printf("Parent: I killed all my children!\n");
+	i = 0;
+	while (i < 3)
+	{
+		waitpid(pid[i], &status, 0);
+		if (WIFEXITED(status))
+			printf("Parent: Child [%d] terminated normally.\n", pid[i]);
+		else if (WIFSIGNALED(status))
+		{
+			printf("Parent: Child [%d] was interrupted.\n", pid[i]);
+			if (WTERMSIG(status) == SIGTERM)
+				printf("\e[31mParent: Child [%d] got the %d signal, SIGTERM\e[0m\n",
+                            pid[i], WTERMSIG(status));
+			 if (WTERMSIG(status) == SIGKILL)
+                printf("\e[31mParent: Child [%d] got the %d signal, SIGKILL\e[0m\n",
+                            pid[i], WTERMSIG(status));
+		}
+		i++;
+	}
+}
 
 int	main(void)
 {
-	pid_t	pid;
-	int		*status;
+	pid_t	pid[3];
+	int		i;
 
-	status = 0;
-	printf("Fork Here.\n");
-	pid = fork();
-
-	if (pid == -1)
-		return (1);
-	if (pid == 0)
+	i = 0;
+	while (i < 3)
 	{
-		printf("this is the child pid %d\n", pid);
-		sleep(2);
-		printf("child done\n");
+		pid[i] = fork();
+		if (pid[i] == -1)
+			return (EXIT_FAILURE);
+		else if (pid[i] == 0)
+			child();
+		else if (pid[i] > 0)
+			printf("Parent: Child #%d created with pid = %d\n", i, pid[i]);
+		usleep(1000);
+		i++;
 	}
-	else if (pid > 0)
-	{
-		printf("this is parent %d\n", pid);
-		waitpid(pid, status, 0);
-		printf("parent done\n");
-	}
-	return (0);
+	kill_and_get_children(pid);
+	return (EXIT_SUCCESS);
 }
